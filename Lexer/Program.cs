ed53e4f093dadba48f7187fe7c661d1a4bf3768a
Aryadev Chavali <aryadevchavali1@gmail.com>
2018-05-25 17:57:47 +0100

    Added lexer support for classes and returns a string

--- Lexer/Program.cs
+++ Lexer/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lexer
 {
@@ -10,6 +7,47 @@ namespace Lexer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Lexer("string[] x = new nig"));
+
+            Console.ReadKey();
+        }
+
+        static string Lexer(string command)
+        {
+            var operators = new[]
+            {
+                "+","-","/","*","**"
+            };
+            var commands = new[]
+            {
+                "print","retrieve","run","clear","exit"
+            };
+
+            var dataTypes = new[]
+            {
+                "string", "int", "new"
+            };
+
+            var classes = new[]
+            {
+                "string","int","string[]","int[]"
+            };
+
+            var split = command.Split(' ');
+            var lexerReturn = "";
+            foreach (string s in split)
+            {
+                if (dataTypes.Contains(s))
+                    lexerReturn += $"{{datatype {s.Trim()}}} ";
+                else if (commands.Contains(s))
+                    lexerReturn += $"{{command {s.Trim()}}} ";
+                else if (operators.Contains(s))
+                    lexerReturn += $"{{operator {s.Trim()}}} ";
+                else if (classes.Contains(s))
+                    lexerReturn += $"{{class {s.Trim()}}} ";
+            }
+
+            return lexerReturn;
         }
     }
 }
