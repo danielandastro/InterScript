c4dc92c399a6357e7ac684859366aadb726b34bf
Daniel Shroff <danielshroff@hotmail.com>
2018-05-26 00:52:29 +0100

    Parser

---
+++ Parser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using LexerLib;
+namespace Parser
+
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Intersharp lex = new Intersharp();
+            string type = "";
+            type = Intersharp.Lexer(Console.ReadLine());
+            type = type.Split('{','}')[1];
+            type = type.Replace("datatype","");
+            Console.WriteLine(type);
+        }
+    }
+}
