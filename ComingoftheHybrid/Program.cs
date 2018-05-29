645ad0f9e34683622c689a40aac8c680c6e31231
Daniel Shroff <danielshroff@hotmail.com>
2018-05-29 23:32:20 +0100

    Added the first data

---
+++ ComingoftheHybrid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ComingoftheHybrid
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parse("run x");
+        }
+
+        public static void Parse(string command)
+        {
+            string keyword, dataType, args, keywordType, varName, varData;
+            var spaceSplit = command.Split(' ');
+            var equalSplit = command.Split('=');
+            if (spaceSplit.Length == 2)
+            {
+                keywordType = "command";
+                keyword = spaceSplit[0];
+                args = spaceSplit[1];
+            }
+            else
+            {
+                keywordType = "declaration";
+                varName = spaceSplit[1];
+                dataType = spaceSplit[0];
+                varData = equalSplit[1];
+            }
+        }
+    }
+}
